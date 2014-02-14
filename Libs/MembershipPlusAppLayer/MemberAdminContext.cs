2a1eb1e287b57eff563bfc6142e9f6754e50d305
Shuqian Ying <sqying@archymeta.com>
2014-02-14 16:17:25 +0800

    Starting member admin ...

---
+++ Libs/MembershipPlusAppLayer/MemberAdminContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptoGateway.RDB.Data.MembershipPlus;
+using Archymeta.Web.MembershipPlus.AppLayer.Models;
+using Archymeta.Web.Security.Resources;
+
+namespace Archymeta.Web.MembershipPlus.AppLayer
+{
+    public class MemberAdminContext
+    {
+        internal static CallContext Cntx
+        {
+            get
+            {
+                return ApplicationContext.ClientContext.CreateCopy();
+            }
+        }
+    }
+}
---
+++ WebApps/MemberAdmin/Controllers/MemberAdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Security.Principal;
+using Microsoft.IdentityModel;
+//using Microsoft.IdentityModel.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using CryptoGateway.RDB.Data.MembershipPlus;
+using Archymeta.Web.Security;
+using Archymeta.Web.Security.Resources;
+using Archymeta.Web.MembershipPlus.AppLayer;
+using Archymeta.Web.MembershipPlus.AppLayer.Models;
+using MemberAdminMvc5.Models;
+
+namespace MemberAdminMvc5.Controllers
+{
+    public class MemberAdminController : BaseController
+    {
+        [HttpGet]
+        [Authorize(Roles = "Administrators")]
+        public ActionResult UserAdmin()
+        {
+            ViewBag.AppName = Startup.App.Name;
+            return View();
+        }
+    }
+}
